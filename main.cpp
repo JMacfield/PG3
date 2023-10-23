cadcc1758b5ab14611b53b61004f5909de1eac08
Lie Shiki <krugerjurgen337@gmail.com>
2023-10-23 10:35:25 +0900

    kadai

--- main.cpp
+++ main.cpp
@@ -8,4 +8,25 @@ uint32_t CalculateRecursivePayment(uint32_t payment, uint32_t hour) {
 	return payment + CalculateRecursivePayment(payment * 2 - 50, hour - 1);
 }
 
-uint32_t Calculate
+uint32_t CalculateGeneralPayment(uint32_t payment, uint32_t hour) {
+	if (hour <= 0) {
+		return payment;
+	}
+
+	return payment + CalculateGeneralPayment(payment, hour - 1);
+}
+
+int main(void) {
+	const uint32_t kHours = 10 - 1;
+	
+	for (uint32_t i = 1; i < kHours; i++) {
+		uint32_t recursivePayment = CalculateRecursivePayment(100, i - 1);
+		uint32_t generalPayment = CalculateGeneralPayment(1072, i - 1);
+
+		std::wcout << i << "時間" << std::endl;
+		std::wcout << "一般的な賃金体制 :" << recursivePayment << std::endl;
+		std::wcout << "再帰的な賃金体制 :" << generalPayment << std::endl;
+	}
+
+	return 0;
+}
