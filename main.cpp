28684fa8d06a3e122533e325db9c8f4b9c4bc68e
Lie Shiki <krugerjurgen337@gmail.com>
2023-11-01 12:56:41 +0900

    assignment 02-04

--- main.cpp
+++ main.cpp
@@ -1,1 +1,21 @@
-﻿
+﻿#include "Circle.h"
+#include "Rectangle.h"
+
+int main(void) {
+	const size_t kShape = 2;
+	IShape* shape[kShape];
+
+	shape[0] = new Circle(8.0f);
+	shape[1] = new Rectangle(4.0f, 11.0f);
+
+	for (size_t i = 0; i < kShape; i++) {
+		shape[i]->Size();
+		shape[i]->Draw();
+	}
+
+	for (size_t i = 0; i < kShape; i++) {
+		delete shape[i];
+	}
+
+	return 0;
+}
