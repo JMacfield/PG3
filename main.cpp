a0fccaaa8b440c5773af4877838a435b361686e1
Lie Shiki <krugerjurgen337@gmail.com>
2023-12-11 11:58:36 +0900

    change UTF-8

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-﻿#include <functional>
+#include <functional>
 #include <iostream>
 #include <stdio.h>
 #include <random>
@@ -24,6 +24,8 @@ int PlayerChoice() {
 }
 
 int main(void) {
+	SetConsoleOutputCP(65001);
+
 	std::random_device rd;
 	std::mt19937 mt(rd());
 
