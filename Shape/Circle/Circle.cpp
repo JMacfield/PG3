56c1ce1b299a1702c06dc59cd78ae031f3bf7c28
Lie Shiki <krugerjurgen337@gmail.com>
2023-12-11 11:54:55 +0900

    assignment

--- Shape/Circle/Circle.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include "Circle.h"
-
-void Circle::Size() {
-	size_ = radius_ * radius_ * 3.14f;
-}
-
-void Circle::Draw() {
-	cout << size_ << endl;
-}
--- Shape/Rectangle/Rectangle.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include "Rectangle.h"
-
-void Rectangle::Size() {
-	size_ = height_ * width_;
-}
-
-void Rectangle::Draw() {
-	cout << size_ << endl;
-}
