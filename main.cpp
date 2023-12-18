a42f97ad2e134b7e8c7d6a1b2b97ec00e57a15ca
Lie Shiki <krugerjurgen337@gmail.com>
2023-12-18 10:35:53 +0900

    assignment

--- main.cpp
+++ main.cpp
@@ -1,27 +1,51 @@
-﻿#include <iostream>
-#include <stdio.h>
-#include <typeinfo>
+﻿#include <stdio.h>
+#include <Windows.h>
 
-template<typename Type>
-Type Min(Type a, Type b) {
-	return a > b ? b : a;
-}
+#include <algorithm>
+#include <cassert>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <list>
 
-template <typename Type>
-void PrintMin(Type a) {
-	std::cout << typeid(a).name() << ";" << a << std::endl;
-}
+struct StudentAccount {
+	std::string name;
+	std::string gradeNumber;
+	std::string attendanceNumber;
+};
 
-template<>
-void PrintMin<char>(char a) {
-	std::cout << "文字列以外は代入できません" << std::endl;
-}
+int main() {
+	std::list<StudentAccount> studentAccounts;
+	// 読み込むファイルを開く
+	std::ifstream inputFile("PG3_05_02.txt");
+	assert(inputFile.is_open());
 
-int main(void) {
-	PrintMin(Min<int>(7, 12));
-	PrintMin(Min<float>(3.0f, 9.0f));
-	PrintMin(Min<double>(static_cast<double>(4.0f), static_cast<double>(3.0f)));
-	PrintMin(Min<char>('y', 't'));
+	// 行ごとに読み込み
+	std::string line;
+	while (getline(inputFile, line)) {
+		// 1行分の文字列をストリームに変換して解析しやすくする
+		std::istringstream lineStream(line);
+		std::string account;
+		while (getline(lineStream, account, ',')) {
+			StudentAccount studentAccount{};
+			studentAccount.name = account;
+			std::string gradeNumber = account.substr(2, 3);
+			std::string attendanceNumber = account.substr(6, 4);
+			studentAccount.gradeNumber = gradeNumber.c_str();
+			studentAccount.attendanceNumber = attendanceNumber.c_str();
+			studentAccounts.emplace_back(studentAccount);
+		}
+	}
 
+	// ファイルを閉じる
+	inputFile.close();
+
+	studentAccounts.sort([](const StudentAccount& a, const StudentAccount& b) {
+		return  std::atoi((a.gradeNumber + a.attendanceNumber).c_str()) < std::atoi((b.gradeNumber + b.attendanceNumber).c_str());
+		});
+
+	for (auto& accountName : studentAccounts) {
+		std::cout << accountName.name << std::endl;
+	}
 	return 0;
 }
