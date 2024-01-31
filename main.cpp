b529425db585dd5357d6274d043d1b57df04e79d
Lie Shiki <krugerjurgen337@gmail.com>
2024-01-31 12:43:29 +0900

    update

--- main.cpp
+++ main.cpp
@@ -1,52 +1,30 @@
-﻿#include <functional>
-#include <iostream>
-#include <stdio.h>
-#include <random>
-#include <windows.h>
-
-void SetTime(std::function<void()> function, int second) {
-	Sleep(second * 1000);
-	function();
-}
-
-int GetUserGuess() {
-	int userGuess = 0;
-
-	while (userGuess != 1 && userGuess != 2) {
-		std::cout << "奇数か偶数か選択しEnterを押してください" << std::endl;
-		std::cout << "1 : 奇数, 2 : 偶数" << std::endl;
-		std::cin >> userGuess;
-	}
-	return userGuess;
-}
+﻿#include <iostream>
+#include <string>
+#include <chrono>
+#include <Windows.h>
 
 int main(void) {
-	std::random_device rd;
-	std::mt19937 mt(rd());
+   // SetConsoleOutputCP(65001);
 
-	int userGuess = GetUserGuess();
-	
-	std::function<int()> diceRoll = [&mt]() {
-		return mt() % 6 + 1;
-		};
+    std::string a(100000, 'a');
 
-	int diceResult = diceRoll();
+    {
+        auto start = std::chrono::steady_clock::now();
+        std::string copy = a;
+        auto end = std::chrono::steady_clock::now();
 
-	std::function<void()> result = [&]() {
-		std::cout << "さいころの出目は " << diceResult;
-		std::cout << " で";
-		std::cout << (diceResult % 2 == 0 ? "偶数" : "奇数");
-		std::cout << " です。" << std::endl;
+        auto duration = end - start;
+        std::cout << "コピーにかかった時間: " << duration.count() << " マイクロ秒" << std::endl;
+    }
+   
+    {
+        auto start = std::chrono::steady_clock::now();
+        std::string move = std::move(a);
+        auto end = std::chrono::steady_clock::now();
 
-		if ((diceResult % 2 == 0 && userGuess == 2) || (diceResult % 2 == 1 && userGuess == 1)) {
-			std::cout << "当たり！" << std::endl;
-		}
-		else {
-			std::cout << "はずれ..." << std::endl;
-		}
-		};
+        auto duration = end - start;
+        std::cout << "移動にかかった時間: " << duration.count() << " マイクロ秒" << std::endl;
+    }
 
-	SetTime(result, 3);
-
-	return 0;
+    return 0;
 }
